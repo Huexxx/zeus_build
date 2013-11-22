5cdcd315693eeb6e5eef9efde0c876252d072ceb
Huexxx <huexxx@gmail.com>
2013-11-22 11:13:45 +0100

    ro.secure=0

--- core/main.mk
+++ core/main.mk
@@ -319,8 +319,8 @@ user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-  # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  # Target is not secure in user builds.
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
